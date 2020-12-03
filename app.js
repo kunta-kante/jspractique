2ee4df3246ac1eefe3df4c4d2478902e4748c326
Rahman <rahmanraynkunta@gmail.com>
2020-12-03 16:27:30 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// const stars = document.querySelectorAll('#list-of-stars li');
+// stars.classList.add('gess')
+// star.addEventListener('click', stars)
+
+// stars.forEach((star) => {
+//     star.classList.add('gess')
+// });
+// for (let i = 0; i < star.length; i++) {
+//     stars[i].addEventListener('click', () => {
+//         for (let a = 0; a < star.length; a++) {
+//             stars.classList.remove('gess')
+//             stars.classList.add('gess')
+//         }
+//         for (let b = 0; b < star.length; b++) {
+//             stars.classList.remove('gess')
+//             stars.classList.add('gess')
+//         }
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const button = document.querySelector('.button')
+//window.setInterval(function() {
+//button.classList.toggle('red')
+//}, 1000)
+//const ps = document.querySelector('#button')
+//window.setInterval(function() {
+//ps.classList.toggle('red')
+//}, 1000)
+//const li = document.querySelector('li');
